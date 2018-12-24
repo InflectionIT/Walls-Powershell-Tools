df2d859eefa509d9a345b8aa45e7c04cd0efc9c9
alanrwest <alanrwest@yahoo.com>
2018-12-24 11:58:18 -0800

    Add files via upload

--- Walls.ps1
+++ Walls.ps1
@@ -288,7 +288,7 @@ function RunDataChecks {
     Write-Host $data.MatterNoKeyMap " Matters with no EntityKeyMap entry"
     Write-Host $data.MatterBadPairing " Matters with mismatched client/matter pairing in EntityKeyMap"
     Write-Host $data.DuplicateEntities " Duplicate user/group/client entities"
-    Write-Host $data.DuplicateMatterEntities "Duplicate matter entities"
+    Write-Host $data.DuplicateMatterEntities " Duplicate matter entities"
     Write-Host $data.MatterBadClients " Matters with bad clients"
     Write-Host $data.EntityBlankSystemID " Entities with blank entityRemoteSystemId"
     Write-Host $data.EntitySpaces " Entity IDs with spaces in the name"
